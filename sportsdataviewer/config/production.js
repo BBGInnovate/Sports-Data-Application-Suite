4ee44690ff4ba12aac1f0e3a32a5fff3647322b6
Lisa Backer <lbacker@bbg.gov>
2014-12-23 11:17:47 -0500

    changed port back to 443

--- sportsdataviewer/config/production.js
+++ sportsdataviewer/config/production.js
@@ -12,7 +12,7 @@ var config = {
 , detailedErrors: true
 , debug: true
 //, hostname: 'soccer-epl-home.voanews.com'
-//, port: 443
+, port: 443
 , model: {
     defaultAdapter: 'filesystem'
 } 
@@ -25,57 +25,8 @@ var config = {
  , jsonDirectory : process.cwd() + '/data/json/'
  , ssl: {
     key: '/etc/apache2/SSL/soccer-epl-home.voanews.com.key'
-  , cert: '/etc/apache2/SSL/soccer-epl-home.voanews.com.crt'
-}
-
-/* // Using Postgres as the default, with only a Postgres DB
-, model: {
-    defaultAdapter: 'postgres'
+    , cert: '/etc/apache2/SSL/soccer-epl-home.voanews.com.crt'
   }
-, db: {
-    postgres: {
-      user: process.env.USER
-    , database: process.env.USER
-    , password: null
-    , host: null
-    , port: 5432
-    }
-  }
-*/
-
-/* // Using MySQL as the default, with only a MySQL DB
-, model: {
-    defaultAdapter: 'mysql'
-  }
-, db: {
-    mysql: {
-      host: 'localhost'
-    , user: process.env.USER
-    , database: process.env.USER
-    , password: null
-    }
-  }
-*/
-
-/* // Using Postgres as the default, with both Postgres and Riak
-, model: {
-    defaultAdapter: 'postgres'
-  }
-, db: {
-    postgres: {
-      user: process.env.USER
-    , database: process.env.USER
-    , password: null
-    , host: null
-    , port: 5432
-    }
-  , riak: {
-      protocol: 'http'
-    , host: 'localhost'
-    , port: 8098
-  }
-  }
-*/
 };
 
 module.exports = config;
