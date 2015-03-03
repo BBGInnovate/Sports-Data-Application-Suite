5d4d76c3dddf59e5979e12c71692ba1fd039aac1
Lisa Backer <lbacker@bbg.gov>
2015-03-03 15:17:39 -0500

    UPDATE: Added Flag for SSL vs non SSL bootup.
    Added a flag to let local development not run with the SSL certs. should be: node server.js false.
    
    We will boot the server with SSL normally.

--- sportsfayserver/server.js
+++ sportsfayserver/server.js
@@ -6,7 +6,7 @@
  */
 
 /* *************************** Required Classes **************************** */
-//var http = require('http');
+var http = require('http');
 var https = require('https');
 var faye = require('faye');
 var fs = require('fs');
@@ -16,27 +16,49 @@ var bayeux = new faye.NodeAdapter({mount: '/faye', timeout: 45});
 
 var secret = '78654323MyVeryL0ngStr1ngTh4tIsC00l4ndYouC4ntT0uchThi5IfY0uTry9907654';
 
+// by default we will ALWAYS run under SSL
+var runUnderSSL = true;
 
-var options = {
-  key: fs.readFileSync('/etc/apache2/SSL/soccer-epl-home.voanews.com.key'),
-  cert: fs.readFileSync('/etc/apache2/SSL/soccer-epl-home.voanews.com.crt'),
-  ca: fs.readFileSync('/etc/apache2/SSL/gd_bundle-g2-g1.crt')
-};
+// if in dev (local) start the server this way: node server.js false
+if (process.argv[2] !== undefined && process.argv[2] === 'false'){
+	runUnderSSL = false;
+}
 
-// Handle non-Bayeux requests
-var server = https.createServer(options, function(request, response) {
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.end('Hello, non-Bayeux request');
-});
+// launch using SSL...
+if( runUnderSSL ){
 
-bayeux.attach( server );
-server.listen( 443 );
+	var options = {
+	  key: fs.readFileSync('/etc/apache2/SSL/soccer-epl-home.voanews.com.key'),
+	  cert: fs.readFileSync('/etc/apache2/SSL/soccer-epl-home.voanews.com.crt'),
+	  ca: fs.readFileSync('/etc/apache2/SSL/gd_bundle-g2-g1.crt')
+	};
+
+	// Handle non-Bayeux requests
+	var server = https.createServer(options, function(request, response) {
+	  response.writeHead(200, {'Content-Type': 'text/plain'});
+	  response.end('Hello, non-Bayeux request');
+	});
+
+	bayeux.attach( server );
+	server.listen( 443 );
+
+ } else { // launch NOT using SSL, local development
+
+	// Handle non-Bayeux requests
+	var server = http.createServer(function(request, response) {
+		response.writeHead(200, {'Content-Type': 'text/plain'});
+		response.end('Hello, non-Bayeux request');
+	});
+
+	bayeux.attach( server );
+	server.listen( 8000 );
+}
+
 
 bayeux.on('publish', function(clientId, channel, data) {
   
   console.log(arguments);
   console.log('something happened');
-
 });
 
 
